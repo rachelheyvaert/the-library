1ed7ffe6e49882acc95f9e5a2eadbf0cc7254c72
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-19 09:23:04 -0500

    Creates full cRUD for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+    def index 
+        render json: Book.all, status: :ok
+    end
+
+    def show
+        book = Book.find(params[:id])
+        render json: book, include: :reviews, status: :ok
+    end 
+    def create 
+        book = Book.create!(book_params)
+        render json: book, status: :created
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update!(book_params)
+        render json: book, status: :accepted
+    end
+
+
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        head :no_content
+    end
+
+    private
+
+    def book_params
+        params.permit(:title, :author, :page_count, :description, :img_url)
+    end
+
+end
+
 end
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :description, :page_count, :img_url
+  attributes :title, :author, :description, :page_count, :img_url
+  has_many :reviews
 end
