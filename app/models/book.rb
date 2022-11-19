cfd4dc961ed02dad4d0923cec73fd76a62c30b2d
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-19 09:06:13 -0500

    Creates tables

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+    has_many :users, through: :reviews
 end
--- db/migrate/20221119135222_create_books.rb
+++ db/migrate/20221119135222_create_books.rb
@@ -1,11 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :title, unique: true
+      t.string :title
       t.string :author
       t.string :description
       t.integer :page_count
-      t.string :img_url, unique: true
+      t.string :img_url
 
       t.timestamps
     end
