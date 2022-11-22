c5586f2be7f3aef1afa2076164fbd38274175ca8
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-22 07:54:08 -0500

    builds import/exports in components

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 function App() {
   return (
     <div className="App">
-      {/* <Navigation /> */}
+ 
       <button>Button</button>
     
     </div>
---
+++ client/src/components/BookContainter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookContainter = () => {
+  return (
+    <div>BookContainter</div>
+  )
+}
+
+export default BookContainter
